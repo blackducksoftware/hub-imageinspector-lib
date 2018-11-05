7d980b0d66b4ff67ff05875b34bd91b357d8e7fd
Steve Billings <sbillings@blackducksoftware.com>
2018-11-05 10:46:24 -0500

    refactoring

--- src/main/java/com/synopsys/integration/blackduck/imageinspector/linux/Os.java
+++ src/main/java/com/synopsys/integration/blackduck/imageinspector/linux/Os.java
@@ -24,6 +24,7 @@
 package com.synopsys.integration.blackduck.imageinspector.linux;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
@@ -69,55 +70,63 @@ public class Os {
     }
 
     public Optional<String> getLinxDistroName(final File etcDirFile) {
-        // TODO this method needs refactoring
         try {
             if ("redhat-release".equals(etcDirFile.getName())) {
-                logger.trace("Found redhat-release");
-                List<String> lines = null;
-                lines = FileUtils.readLines(etcDirFile, StandardCharsets.UTF_8);
-                if (lines.size() > 0) {
-                    String line = lines.get(0);
-                    if (line.startsWith("Red Hat")) {
-                        logger.trace("Contents of redhat-release indicate RHEL");
-                        return Optional.of(OperatingSystemEnum.RHEL.name().toLowerCase());
-                    }
-                    if (line.startsWith("CentOS")) {
-                        logger.trace("Contents of redhat-release indicate CentOS");
-                        return Optional.of(OperatingSystemEnum.CENTOS.name().toLowerCase());
-                    }
-                    logger.warn(String.format("Found redhat-release file %s but don't understand the contents: '%s'", etcDirFile.getAbsolutePath(), line));
-                    return Optional.empty();
-                }
-                logger.warn(String.format("Unable to discern linux distro from %s", etcDirFile.getAbsolutePath()));
-                return Optional.empty();
-            }
-            String linePrefix = null;
-            if ("lsb-release".equals(etcDirFile.getName())) {
-                logger.trace("Found lsb-release");
-                linePrefix = "DISTRIB_ID=";
-            } else if ("os-release".equals(etcDirFile.getName())) {
-                logger.trace("Found os-release");
-                linePrefix = "ID=";
+                return getLinuxDistroNameFromRedHatReleaseFile(etcDirFile);
             } else {
-                logger.warn(String.format("File %s is not a Linux distribution-identifying file", etcDirFile.getAbsolutePath()));
-                return Optional.empty();
+                return getLinuxDistroNameFromStandardReleaseFile(etcDirFile);
             }
-            List<String> lines = null;
-            lines = FileUtils.readLines(etcDirFile, StandardCharsets.UTF_8);
-            for (String line : lines) {
-                line = line.trim();
-                if (line.startsWith(linePrefix)) {
-                    final String[] parts = line.split("=");
-                    final String distroName = parts[1].replaceAll("\"", "").toLowerCase();
-                    logger.info(String.format("Found target image Linux distro name '%s' in file %s", distroName, etcDirFile.getAbsolutePath()));
-                    return Optional.of(distroName);
-                }
-            }
-            logger.warn(String.format("Did not find value for %s in %s", linePrefix, etcDirFile.getAbsolutePath()));
-            return Optional.empty();
         } catch (final Exception e) {
             logger.warn(String.format("Error reading or parsing Linux distribution-identifying file: %s: %s", etcDirFile, e.getMessage()));
             return Optional.empty();
         }
+    }
+
+    private Optional<String> getLinuxDistroNameFromStandardReleaseFile(final File etcDirFile) throws IOException {
+        String linePrefix = null;
+        if ("lsb-release".equals(etcDirFile.getName())) {
+            logger.trace("Found lsb-release");
+            linePrefix = "DISTRIB_ID=";
+        } else if ("os-release".equals(etcDirFile.getName())) {
+            logger.trace("Found os-release");
+            linePrefix = "ID=";
+        } else {
+            logger.warn(String.format("File %s is not a Linux distribution-identifying file", etcDirFile.getAbsolutePath()));
+            return Optional.empty();
+        }
+        List<String> lines = null;
+        lines = FileUtils.readLines(etcDirFile, StandardCharsets.UTF_8);
+        for (String line : lines) {
+            line = line.trim();
+            if (line.startsWith(linePrefix)) {
+                final String[] parts = line.split("=");
+                final String distroName = parts[1].replaceAll("\"", "").toLowerCase();
+                logger.info(String.format("Found target image Linux distro name '%s' in file %s", distroName, etcDirFile.getAbsolutePath()));
+                return Optional.of(distroName);
+            }
+        }
+        logger.warn(String.format("Did not find value for %s in %s", linePrefix, etcDirFile.getAbsolutePath()));
+        return Optional.empty();
+    }
+
+    private Optional<String> getLinuxDistroNameFromRedHatReleaseFile(final File etcDirFile) throws IOException {
+        logger.trace("Found redhat-release");
+        List<String> lines = null;
+        lines = FileUtils.readLines(etcDirFile, StandardCharsets.UTF_8);
+        if (lines.size() > 0) {
+            String line = lines.get(0);
+            if (line.startsWith("Red Hat")) {
+                logger.trace("Contents of redhat-release indicate RHEL");
+                return Optional.of(OperatingSystemEnum.RHEL.name().toLowerCase());
+            }
+            if (line.startsWith("CentOS")) {
+                logger.trace("Contents of redhat-release indicate CentOS");
+                return Optional.of(OperatingSystemEnum.CENTOS.name().toLowerCase());
+            }
+            logger.warn(String.format("Found redhat-release file %s but don't understand the contents: '%s'", etcDirFile.getAbsolutePath(), line));
+            return Optional.empty();
+        }
+        logger.warn(String.format("Unable to discern linux distro from %s", etcDirFile.getAbsolutePath()));
+        return Optional.empty();
     }
 }
