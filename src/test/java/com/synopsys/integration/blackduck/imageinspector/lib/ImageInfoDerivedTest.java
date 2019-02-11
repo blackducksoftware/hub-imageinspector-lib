1c1cb90e19941f5397fc3ae98d61243ef7609718
Steve Billings <sbillings@blackducksoftware.com>
2019-02-11 13:09:27 -0500

    finished ImageInfoDerivedTest

--- src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInfoDerivedTest.java
+++ src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInfoDerivedTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.synopsys.integration.bdio.model.BdioProject;
 import com.synopsys.integration.bdio.model.SimpleBdioDocument;
+import com.synopsys.integration.blackduck.imageinspector.api.OperatingSystemEnum;
 import com.synopsys.integration.blackduck.imageinspector.api.PackageManagerEnum;
 import com.synopsys.integration.blackduck.imageinspector.imageformat.docker.manifest.ManifestLayerMapping;
 import java.io.File;
@@ -38,7 +39,12 @@ public class ImageInfoDerivedTest {
     derived.setManifestLayerMapping(mapping);
 
     assertEquals("testLayer", derived.getManifestLayerMapping().getLayers().get(0));
-    // TODO lots more to check on derived...
+    assertEquals("testCodelocationName", derived.getCodeLocationName());
+    assertEquals("testFinalProjectName", derived.getFinalProjectName());
+    assertEquals("testFinalProjectVersionName", derived.getFinalProjectVersionName());
+    assertEquals("testProjectName", derived.getBdioDocument().project.name);
+    assertEquals(
+        OperatingSystemEnum.ALPINE, derived.getImageInfoParsed().getPkgMgr().getPackageManager().getInspectorOperatingSystem());
   }
 
 }
