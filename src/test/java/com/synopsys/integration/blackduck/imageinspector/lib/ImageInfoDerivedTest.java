4e187426d1f91ab3a9ef7284666fd2d0fbb79a2b
Steve Billings <sbillings@blackducksoftware.com>
2019-02-11 11:05:06 -0500

    working on tests

--- src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInfoDerivedTest.java
+++ src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInfoDerivedTest.java
@@ -2,8 +2,12 @@ package com.synopsys.integration.blackduck.imageinspector.lib;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.synopsys.integration.bdio.model.BdioProject;
+import com.synopsys.integration.bdio.model.SimpleBdioDocument;
 import com.synopsys.integration.blackduck.imageinspector.api.PackageManagerEnum;
+import com.synopsys.integration.blackduck.imageinspector.imageformat.docker.manifest.ManifestLayerMapping;
 import java.io.File;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class ImageInfoDerivedTest {
@@ -19,6 +23,22 @@ public class ImageInfoDerivedTest {
     assertEquals("alpine", parsed.getLinuxDistroName());
     assertEquals(PackageManagerEnum.APK, parsed.getPkgMgr().getPackageManager());
     assertEquals("apk", parsed.getPkgMgr().getExtractedPackageManagerDirectory().getName());
+
+    ImageInfoDerived derived = new ImageInfoDerived(parsed);
+    derived.setCodeLocationName("testCodelocationName");
+    SimpleBdioDocument bdioDoc = new SimpleBdioDocument();
+    bdioDoc.project = new BdioProject();
+    bdioDoc.project.name = "testProjectName";
+    assertEquals("testProjectName", bdioDoc.project.name);
+    derived.setBdioDocument(bdioDoc);
+    derived.setFinalProjectName("testFinalProjectName");
+    derived.setFinalProjectVersionName("testFinalProjectVersionName");
+    ManifestLayerMapping mapping = new ManifestLayerMapping("testImageName", "testTagName", "testConfig", Arrays
+        .asList("testLayer"));
+    derived.setManifestLayerMapping(mapping);
+
+    assertEquals("testLayer", derived.getManifestLayerMapping().getLayers().get(0));
+    // TODO lots more to check on derived...
   }
 
 }
