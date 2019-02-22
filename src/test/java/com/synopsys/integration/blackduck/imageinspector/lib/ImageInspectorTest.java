d08cead00e44e62081d0c7be57830f48a7581620
Steve Billings <sbillings@blackducksoftware.com>
2019-02-22 14:18:50 -0500

    finished splitting up ImageInspectorTest

--- src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInspectorTest.java
+++ src/test/java/com/synopsys/integration/blackduck/imageinspector/lib/ImageInspectorTest.java
@@ -17,6 +17,7 @@ import org.mockito.Mockito;
 import com.google.gson.GsonBuilder;
 import com.synopsys.integration.blackduck.imageinspector.api.ImageInspectorOsEnum;
 import com.synopsys.integration.blackduck.imageinspector.api.PackageManagerEnum;
+import com.synopsys.integration.blackduck.imageinspector.api.WrongInspectorOsException;
 import com.synopsys.integration.blackduck.imageinspector.api.name.Names;
 import com.synopsys.integration.blackduck.imageinspector.imageformat.docker.DockerTarParser;
 import com.synopsys.integration.blackduck.imageinspector.linux.FileOperations;
@@ -40,61 +41,6 @@ public class ImageInspectorTest {
     }
 
     @Test
-    public void testAll() throws IOException, IntegrationException {
-
-        final File workingDir = new File("src/test/resources/working");
-        final File tarExtractionDirectory = imageInspector.getTarExtractionDirectory(workingDir);
-        assertTrue(tarExtractionDirectory.getAbsolutePath().endsWith("src/test/resources/working/tarExtraction"));
-
-        final File dockerTarfile = new File("src/test/resources/testDockerTarfile");
-        final List<File> layerTars = imageInspector.extractLayerTars(tarExtractionDirectory, dockerTarfile);
-        Mockito.verify(tarParser).unPackImageTar(tarExtractionDirectory, dockerTarfile);
-
-        final GsonBuilder gsonBuilder = new GsonBuilder();
-        final String imageRepo = "alpine";
-        final String imageTag = "latest";
-        imageInspector.getLayerMapping(gsonBuilder, tarExtractionDirectory, dockerTarfile.getName(), imageRepo, imageTag);
-        Mockito.verify(tarParser).getLayerMapping(gsonBuilder, tarExtractionDirectory, dockerTarfile.getName(), imageRepo, imageTag);
-
-        final String imageConfigFileContents = "testConfig";
-        final List<String> layers = new ArrayList<>();
-        layers.add("testLayer1");
-        layers.add("testLayer2");
-        final ManifestLayerMapping manifestLayerMapping = new ManifestLayerMapping(imageRepo, imageTag, imageConfigFileContents, layers);
-        imageInspector.createInitialImageComponentHierarchy(workingDir, tarExtractionDirectory, dockerTarfile.getName(), manifestLayerMapping);
-        Mockito.verify(tarParser).createInitialImageComponentHierarchy(tarExtractionDirectory, dockerTarfile.getName(), manifestLayerMapping);
-
-        final File targetImageFileSystemParentDir = new File(tarExtractionDirectory, ImageInspector.TARGET_IMAGE_FILESYSTEM_PARENT_DIR);
-        final File targetImageFileSystemRootDir = new File(targetImageFileSystemParentDir, Names.getTargetImageFileSystemRootDirName(imageRepo, imageTag));
-        final String manifestFileContents = FileUtils.readFileToString(new File("src/test/resources/extraction/alpine.tar/manifest.json"), StandardCharsets.UTF_8);
-        final ImageComponentHierarchy imageComponentHierarchy = new ImageComponentHierarchy( manifestFileContents, imageConfigFileContents);
-            imageInspector.extractDockerLayers(ImageInspectorOsEnum.ALPINE, imageComponentHierarchy, targetImageFileSystemRootDir, layerTars, manifestLayerMapping);
-        Mockito.verify(tarParser).extractImageLayers(componentExtractorFactory, ImageInspectorOsEnum.ALPINE, imageComponentHierarchy, targetImageFileSystemRootDir, layerTars, manifestLayerMapping);
-
-        final BdioGenerator bdioGenerator = new BdioGenerator();
-        final String blackDuckProjectName = "testProjectName";
-        final String blackDuckProjectVersion = "testProjectVersion";
-        final String codeLocationPrefix = "testCodeLocationPrefix";
-        final boolean organizeComponentsByLayer = true;
-        final boolean includeRemovedComponents = true;
-        final File extractedPackageManagerDirectory = new File("src/test/resources/imageDir/alpine/lib/apk");
-        final ImagePkgMgrDatabase imagePkgMgrDatabase = new ImagePkgMgrDatabase(extractedPackageManagerDirectory, PackageManagerEnum.APK);
-        final String linuxDistroName = "alpine";
-        final PkgMgr pkgMgr = new ApkPkgMgr(new FileOperations());
-        final ImageInfoParsed imageInfoParsed = new ImageInfoParsed(targetImageFileSystemRootDir, imagePkgMgrDatabase, linuxDistroName, pkgMgr);
-        final ImageInfoDerived imageInfoDerived = imageInspector.generateBdioFromGivenComponents(bdioGenerator, imageInfoParsed, imageComponentHierarchy, manifestLayerMapping, blackDuckProjectName, blackDuckProjectVersion,
-            codeLocationPrefix, organizeComponentsByLayer, includeRemovedComponents);
-        assertEquals(blackDuckProjectName, imageInfoDerived.getFinalProjectName());
-        assertEquals(blackDuckProjectVersion, imageInfoDerived.getFinalProjectVersionName());
-        assertEquals("testCodeLocationPrefix_alpine_latest_APK", imageInfoDerived.getCodeLocationName());
-        assertEquals("APK", imageInfoDerived.getImageInfoParsed().getImagePkgMgrDatabase().getPackageManager().name());
-        assertEquals(imageRepo, imageInfoDerived.getManifestLayerMapping().getImageName());
-        assertEquals(layers.get(1), imageInfoDerived.getManifestLayerMapping().getLayerInternalIds().get(1));
-        assertEquals(String.format("%s/%s", blackDuckProjectName, blackDuckProjectVersion), imageInfoDerived.getBdioDocument().project.bdioExternalIdentifier.externalId);
-    }
-
-
-    @Test
     public void testGetTarExtractionDirectory() {
         final File workingDir = new File("src/test/resources/working");
         final File tarExtractionDirectory = imageInspector.getTarExtractionDirectory(workingDir);
@@ -108,8 +54,7 @@ public class ImageInspectorTest {
         imageInspector.extractLayerTars(tarExtractionDirectory, dockerTarfile);
         Mockito.verify(tarParser).unPackImageTar(tarExtractionDirectory, dockerTarfile);
     }
-
-
+    
     @Test
     public void testGetLayerMapping() throws IOException, IntegrationException {
         final File tarExtractionDirectory = new File("src/test/resources/working/tarExtraction");
@@ -140,5 +85,66 @@ public class ImageInspectorTest {
         final ManifestLayerMapping manifestLayerMapping = new ManifestLayerMapping(imageRepo, imageTag, imageConfigFileContents, layers);
         imageInspector.createInitialImageComponentHierarchy(workingDir, tarExtractionDirectory, dockerTarfile.getName(), manifestLayerMapping);
         Mockito.verify(tarParser).createInitialImageComponentHierarchy(tarExtractionDirectory, dockerTarfile.getName(), manifestLayerMapping);
+    }
+
+    @Test
+    public void testExtractDockerLayers() throws IOException, WrongInspectorOsException {
+        final File tarExtractionDirectory = new File("src/test/resources/working/tarExtraction");
+        final File dockerTarfile = new File("src/test/resources/testDockerTarfile");
+        final String imageRepo = "alpine";
+        final String imageTag = "latest";
+        final String imageConfigFileContents = "testConfig";
+        final List<String> layers = new ArrayList<>();
+        layers.add("testLayer1");
+        layers.add("testLayer2");
+        final ManifestLayerMapping manifestLayerMapping = new ManifestLayerMapping(imageRepo, imageTag, imageConfigFileContents, layers);
+        final List<File> layerTars = new ArrayList<>();
+        final File layerTar = new File(tarExtractionDirectory, String.format("%s/aaa/layer.tar", dockerTarfile.getName()));
+        layerTars.add(layerTar);
+
+        final File targetImageFileSystemParentDir = new File(tarExtractionDirectory, ImageInspector.TARGET_IMAGE_FILESYSTEM_PARENT_DIR);
+        final File targetImageFileSystemRootDir = new File(targetImageFileSystemParentDir, Names.getTargetImageFileSystemRootDirName(imageRepo, imageTag));
+        final String manifestFileContents = FileUtils.readFileToString(new File("src/test/resources/extraction/alpine.tar/manifest.json"), StandardCharsets.UTF_8);
+        final ImageComponentHierarchy imageComponentHierarchy = new ImageComponentHierarchy( manifestFileContents, imageConfigFileContents);
+        imageInspector.extractDockerLayers(ImageInspectorOsEnum.ALPINE, imageComponentHierarchy, targetImageFileSystemRootDir, layerTars, manifestLayerMapping);
+        Mockito.verify(tarParser).extractImageLayers(componentExtractorFactory, ImageInspectorOsEnum.ALPINE, imageComponentHierarchy, targetImageFileSystemRootDir, layerTars, manifestLayerMapping);
+    }
+
+    @Test
+    public void testGenerateBdioFromGivenComponents() throws IOException, IntegrationException {
+
+        final File workingDir = new File("src/test/resources/working");
+        final File tarExtractionDirectory = imageInspector.getTarExtractionDirectory(workingDir);
+        final String imageRepo = "alpine";
+        final String imageTag = "latest";
+        final String imageConfigFileContents = "testConfig";
+        final List<String> layers = new ArrayList<>();
+        layers.add("testLayer1");
+        layers.add("testLayer2");
+        final ManifestLayerMapping manifestLayerMapping = new ManifestLayerMapping(imageRepo, imageTag, imageConfigFileContents, layers);
+        final File targetImageFileSystemParentDir = new File(tarExtractionDirectory, ImageInspector.TARGET_IMAGE_FILESYSTEM_PARENT_DIR);
+        final File targetImageFileSystemRootDir = new File(targetImageFileSystemParentDir, Names.getTargetImageFileSystemRootDirName(imageRepo, imageTag));
+        final String manifestFileContents = FileUtils.readFileToString(new File("src/test/resources/extraction/alpine.tar/manifest.json"), StandardCharsets.UTF_8);
+        final ImageComponentHierarchy imageComponentHierarchy = new ImageComponentHierarchy( manifestFileContents, imageConfigFileContents);
+        final BdioGenerator bdioGenerator = new BdioGenerator();
+        final String blackDuckProjectName = "testProjectName";
+        final String blackDuckProjectVersion = "testProjectVersion";
+        final String codeLocationPrefix = "testCodeLocationPrefix";
+        final boolean organizeComponentsByLayer = true;
+        final boolean includeRemovedComponents = true;
+        final File extractedPackageManagerDirectory = new File("src/test/resources/imageDir/alpine/lib/apk");
+        final ImagePkgMgrDatabase imagePkgMgrDatabase = new ImagePkgMgrDatabase(extractedPackageManagerDirectory, PackageManagerEnum.APK);
+        final String linuxDistroName = "alpine";
+        final PkgMgr pkgMgr = new ApkPkgMgr(new FileOperations());
+        final ImageInfoParsed imageInfoParsed = new ImageInfoParsed(targetImageFileSystemRootDir, imagePkgMgrDatabase, linuxDistroName, pkgMgr);
+        final ImageInfoDerived imageInfoDerived = imageInspector.generateBdioFromGivenComponents(bdioGenerator, imageInfoParsed, imageComponentHierarchy, manifestLayerMapping, blackDuckProjectName, blackDuckProjectVersion,
+            codeLocationPrefix, organizeComponentsByLayer, includeRemovedComponents);
+        assertEquals(blackDuckProjectName, imageInfoDerived.getFinalProjectName());
+        assertEquals(blackDuckProjectVersion, imageInfoDerived.getFinalProjectVersionName());
+        assertEquals("testCodeLocationPrefix_alpine_latest_APK", imageInfoDerived.getCodeLocationName());
+        assertEquals("APK", imageInfoDerived.getImageInfoParsed().getImagePkgMgrDatabase().getPackageManager().name());
+        assertEquals(imageRepo, imageInfoDerived.getManifestLayerMapping().getImageName());
+        assertEquals(layers.get(1), imageInfoDerived.getManifestLayerMapping().getLayerInternalIds().get(1));
+        assertEquals(String.format("%s/%s", blackDuckProjectName, blackDuckProjectVersion), imageInfoDerived.getBdioDocument().project.bdioExternalIdentifier.externalId);
     }
 }
