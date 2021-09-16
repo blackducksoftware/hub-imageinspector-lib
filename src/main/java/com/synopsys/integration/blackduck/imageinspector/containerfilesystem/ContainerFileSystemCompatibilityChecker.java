be436eadb25c0d930aa3e428cc8188f9c15e7f28
Steve Billings <billings@synopsys.com>
2021-09-16 16:38:22 -0400

    style: log a warning if no linux pkg mgr found

--- src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/ContainerFileSystemCompatibilityChecker.java
+++ src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/ContainerFileSystemCompatibilityChecker.java
@@ -10,13 +10,17 @@ package com.synopsys.integration.blackduck.imageinspector.containerfilesystem;
 import com.synopsys.integration.blackduck.imageinspector.containerfilesystem.pkgmgr.pkgmgrdb.PackageManagerToImageInspectorOsMapping;
 import com.synopsys.integration.blackduck.imageinspector.api.ImageInspectorOsEnum;
 import com.synopsys.integration.blackduck.imageinspector.api.WrongInspectorOsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContainerFileSystemCompatibilityChecker {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public void checkInspectorOs(ContainerFileSystemWithPkgMgrDb containerFileSystemWithPkgMgrDb, ImageInspectorOsEnum currentOs) throws WrongInspectorOsException {
         if (containerFileSystemWithPkgMgrDb.getPkgMgr() == null) {
+            logger.warn("No linux package manager was found in this image");
             return; // if there's no pkg mgr in target image, any image inspector will do
         }
         final ImageInspectorOsEnum neededInspectorOs = PackageManagerToImageInspectorOsMapping
