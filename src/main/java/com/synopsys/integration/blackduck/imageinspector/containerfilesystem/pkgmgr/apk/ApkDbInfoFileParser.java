30c77e98287fb53a6f8409d954d407ebc2f48da7
blackduck-serv-builder <serv-builder@blackducksoftware.com>
2022-02-02 10:57:21 -0500

    Using the next snapshot post release 14.1.1-SNAPSHOT

--- src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/pkgmgr/apk/ApkDbInfoFileParser.java
+++ src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/pkgmgr/apk/ApkDbInfoFileParser.java
@@ -1,3 +1,10 @@
+/*
+ * hub-imageinspector-lib
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.imageinspector.containerfilesystem.pkgmgr.apk;
 
 import java.io.File;
--- src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/pkgmgr/pkgmgrdb/DbRelationshipInfo.java
+++ src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/pkgmgr/pkgmgrdb/DbRelationshipInfo.java
@@ -1,3 +1,10 @@
+/*
+ * hub-imageinspector-lib
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.imageinspector.containerfilesystem.pkgmgr.pkgmgrdb;
 
 import java.util.List;
