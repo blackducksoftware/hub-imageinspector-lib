98b551c9da83999a65773e99a0ac15d5f5f8fc0d
Madhu Gopanna <madhusudan.gopanna@synopsys.com>
2024-01-25 13:07:30 -0500

    Fix for IDETECT-4058

--- src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/DataStripper.java
+++ src/main/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/DataStripper.java
@@ -3,7 +3,7 @@ package com.synopsys.integration.blackduck.imageinspector.containerfilesystem;
 public class DataStripper {
     
     public static String stripEpocFromVersion(String version) {
-        return version.substring(2);
+        return version != null && version.startsWith("0:") ? version.substring(2) : version;
     }
     
     public static String stripEpochFromExternalId(String externalId) {
@@ -12,6 +12,13 @@ public class DataStripper {
             return externalId.substring(0, pos + 1).concat(externalId.substring(pos + 3));
         }
         return externalId;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(stripEpocFromVersion("1.62.0-r5"));
+        System.out.println(stripEpocFromVersion("0:1.62.0-r5"));
+        System.out.println(stripEpochFromExternalId("name/0:7.0.0"));
+        System.out.println(stripEpochFromExternalId("name/1:7.0.0"));
     }
     
 }
---
+++ src/test/java/com/synopsys/integration/blackduck/imageinspector/containerfilesystem/DataStripperTest.java
@@ -0,0 +1,27 @@
+package com.synopsys.integration.blackduck.imageinspector.containerfilesystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DataStripperTest {
+    
+    @Test
+    void stripEpocFromVersionTest() {
+        assertEquals(null, DataStripper.stripEpocFromVersion(null));
+        assertEquals("", DataStripper.stripEpocFromVersion(""));
+        assertEquals("1:0:", DataStripper.stripEpocFromVersion("1:0:"));
+        assertEquals("0:", DataStripper.stripEpocFromVersion("0:0:"));
+        assertEquals(" ", DataStripper.stripEpocFromVersion(" "));
+        assertEquals("1.62.0-r5", DataStripper.stripEpocFromVersion("1.62.0-r5"));
+        assertEquals("1.62.0-r5", DataStripper.stripEpocFromVersion("0:1.62.0-r5"));
+    }
+    
+    @Test
+    void stripEpochFromExternalIdTest() {
+        assertEquals("name/:7.0.0", DataStripper.stripEpochFromExternalId("name/:7.0.0"));
+        assertEquals("/7.0.0", DataStripper.stripEpochFromExternalId("/7.0.0"));
+        assertEquals("name/7.0.0", DataStripper.stripEpochFromExternalId("name/0:7.0.0"));
+        assertEquals("name/1:7.0.0", DataStripper.stripEpochFromExternalId("name/1:7.0.0"));
+    }
+    
+}
